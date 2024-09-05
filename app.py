d26bf21f00aaa5d21fcc10febeb040d25ac3a60a
Aaron Zhen <169063331+SNDZIL@users.noreply.github.com>
2024-09-05 13:14:20 +0000

    12

--- app.py
+++ app.py
@@ -24,6 +24,33 @@ def main():
     conn.close()
     return render_template("main.html", r = r )
 
+@app.route('/admin',methods=["get","post"])
+def admin():
+    return render_template('admin.html')
+
+@app.route('/viewDB',methods=["get","post"])
+def viewDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r += str(row)+"\n"
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('viewDB.html', r=r)
+
+@app.route('/deleteDB',methods=["get","post"])
+def deleteDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('deleteDB.html')
+
 @app.route('/store_money',methods=["get","post"])
 def store_money():
     return render_template('store_money.html')
