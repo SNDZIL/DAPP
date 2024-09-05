3ebdf3ece2d20705b6ce51b4b895bf4c3fad6729
Aaron Zhen <169063331+SNDZIL@users.noreply.github.com>
2024-09-05 12:39:42 +0000

    4

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+import sqlite3
+import datetime
 
 app=Flask(__name__)
 
@@ -8,7 +10,19 @@ def index():
 
 @app.route('/main', methods=['get', 'post'])
 def main():
-    return render_template("main.html")
+    r = request.form.get("q")
+    current_time = datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values(?, ?)",(r, current_time))
+    conn.commit()
+    # row = ""
+    # for row1 in c:
+    # row += str(row1)+"\n"
+    # print(row)
+    c.close()
+    conn.close()
+    return render_template("main.html", r = r )
 
 @app.route('/store_money',methods=["get","post"])
 def store_money():
